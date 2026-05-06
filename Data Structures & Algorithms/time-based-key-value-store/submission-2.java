5686f6a07f1c9e01f6a7f93547db49a1205e56bf
UnfamousThomas <31453763+UnfamousThomas@users.noreply.github.com>
2026-05-06 10:17:14 +0300

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.java
@@ -0,0 +1,49 @@
+class TimeMap {
+        private Map<String, List<Data>> values = new HashMap<>();
+    public TimeMap() {}
+    
+    public void set(String key, String value, int timestamp) {
+        List<Data> datas = values.getOrDefault(key, new ArrayList<Data>());
+        datas.add(new Data(timestamp, value));
+        values.put(key, datas);
+    }
+    
+    public String get(String key, int timestamp) {
+        List<Data> datas = values.get(key);
+        if(datas == null || datas.isEmpty()) return "";
+
+        int lo = 0;
+        int hi = datas.size() - 1;
+        String result = "";
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+            Data midElement = datas.get(mid);
+            if(midElement.getTimestamp() <= timestamp) {
+                //matches, but not neccessarily MOST RECENT. Most recent would probably be LARGER
+                result = midElement.getValue();
+                lo = mid + 1;
+            }  else {
+                //does not match
+                hi = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    class Data {
+        private int timestamp;
+        private String value;
+
+        Data(int timestamp, String value) {
+            this.timestamp = timestamp;
+            this.value = value;
+        }
+
+        public int getTimestamp() {
+            return timestamp;
+        }
+        private String getValue() {
+            return value;
+        }
+    }
+}
