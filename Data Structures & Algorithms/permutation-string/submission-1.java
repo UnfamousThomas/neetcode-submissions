7e597d7acccda2b73da54c06db1c4b551fdfc236
UnfamousThomas <31453763+UnfamousThomas@users.noreply.github.com>
2026-05-15 07:35:28 +0300

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int left = 0;
+        int right = s1.length();
+        int[] s1Freq = new int[26];
+        int[] currentWindowFreq = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            s1Freq[c - 'a']++;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            currentWindowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        if(Arrays.equals(currentWindowFreq, s1Freq)) return true;
+
+        while (right < s2.length()) {
+            currentWindowFreq[s2.charAt(right) - 'a']++;
+            currentWindowFreq[s2.charAt(left) - 'a']--;
+            if (Arrays.equals(currentWindowFreq, s1Freq))
+                return true;
+            right++;
+            left++;
+        }
+        return false;
+    }
+}
