333088bce5b6496b86f5a2d2e158db52376aa941
UnfamousThomas <31453763+UnfamousThomas@users.noreply.github.com>
2026-05-17 08:51:17 +0300

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int right = k;
+        List<Integer> finalMaximums = new ArrayList<>(); //Store maximums for each window
+        Deque<Integer> maximums = new ArrayDeque<>(); //Store running maximums
+        for (int i = 0; i < k; i++) {
+            while (!maximums.isEmpty() && nums[maximums.peekLast()] < nums[i]) { //If maximums is not empty
+            //and last in the deque is smaller than i
+                maximums.removeLast(); //remove the last
+            }
+            maximums.addLast(i); //Add new last
+        }
+        finalMaximums.add(nums[maximums.peekFirst()]); //Add the first window to the result
+
+        while (right < nums.length) {
+            while (!maximums.isEmpty() && nums[maximums.peekLast()] < nums[right]) {
+                //Remove last if maximums is not empty, and right pointer num is bigger than currently stored
+                //last element
+                maximums.removeLast();
+            }
+            maximums.addLast(right); //Add new last
+            if(!maximums.isEmpty() && maximums.peekFirst() < right - k + 1) {
+                //Remove first, if maximum index is outside the window
+                // window left boundary is always right - k + 1
+                maximums.removeFirst();
+            }
+            finalMaximums.add(nums[maximums.peekFirst()]); //Add to the result
+            right++;
+        }
+        return finalMaximums.stream().mapToInt(Integer::intValue).toArray(); //convert to array
+    }
+}
