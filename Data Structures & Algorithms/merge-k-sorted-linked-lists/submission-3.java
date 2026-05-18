6b383a28b41c2b6b742d17697342fc1f5a9d485a
UnfamousThomas <31453763+UnfamousThomas@users.noreply.github.com>
2026-05-18 16:02:03 +0300

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) {
+            return null;
+        }
+
+        for(int i = 1; i < lists.length; i++) {
+            lists[i] = mergeTwoLists(lists[i], lists[i-1]);
+        }
+
+        return lists[lists.length - 1];
+    }
+
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode leftPointer = list1; //iterator for list 1
+        ListNode rightPointer = list2; //iterator for list 2
+        ListNode dummy = new ListNode(0); //Dummy for knowing what to return
+        ListNode current = dummy; //Last processed element
+
+        while (leftPointer != null || rightPointer != null) {
+            if (leftPointer == null) { //If left is null, use right and return
+                // use right pointer
+                current.next = rightPointer;
+                rightPointer = rightPointer.next;
+                return dummy.next;
+            }
+            if (rightPointer == null) { //If right is null, use left and return
+                // use left pointer
+                current.next = leftPointer;
+                leftPointer = leftPointer.next;
+                return dummy.next;
+            }
+            if (leftPointer.val > rightPointer.val) { //If left is bigger, use right
+                // use right
+                current.next = rightPointer; //Set next element to the right
+                current = rightPointer; //Set last processed
+                rightPointer = rightPointer.next; //Move right
+            } else {
+                // use left
+                current.next = leftPointer; //Set next element to the left
+                current = leftPointer; //Set last processed
+                leftPointer = leftPointer.next; //Move left
+            }
+        }
+        return dummy.next; //return first real element
+    }
+}
