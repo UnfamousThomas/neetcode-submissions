020e8f58a85ff10dc4ae1a359aa63534752bd8a7
UnfamousThomas <31453763+UnfamousThomas@users.noreply.github.com>
2026-06-22 20:54:14 +0300

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    //The thing to think of here is to sort them by start time.
+    // Then, we just need to compare every meeting to the next one
+    // If next one starts before current starts, they overlap and thus cannot be both attended
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a,b) -> Integer.compare(a.start, b.start)); 
+
+        for(int i = 0; i < intervals.size()-1; i++) {
+            Interval current = intervals.get(i);
+            Interval next = intervals.get(i+1);
+
+            if(current.end > next.start) return false;
+        }
+        return true;
+    }
+}
